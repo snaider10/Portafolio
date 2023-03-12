545e76ad107a96d5fc07574f38e017e002ed25f3
Darkin03 <esnaiderortega@unisinu.edu.co>
2023-03-12 13:00:15 -0500

    delete

--- src/components/Atajo.jsx
+++
@@ -1,8 +0,0 @@
-
-export const Atajo = (props) => {
-    return(
-        <a className="atajos-link" href= {props.link}>
-            <label><i className= {props.icono}></i></label>
-        </a>
-    )    
-}
